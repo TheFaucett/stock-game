ad16598c938ae4affa753ee00ae2770a6936add0
TheFaucett <157230745+TheFaucett@users.noreply.github.com>
2025-07-18 15:47:42 -0500

    Update apiConfig.js

--- stock-game/frontend/src/apiConfig.js
+++ stock-game/frontend/src/apiConfig.js
@@ -1,2 +1,3 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000"; // For Next.js and Vercel
+console.log("All envs:", Object.entries(process.env).filter(([k]) => k.startsWith('NEXT_PUBLIC_')));
 export default API_BASE_URL;
